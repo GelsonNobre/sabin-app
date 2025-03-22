9bafb01fd88058216b0d34603bbf0d4900546c75
JoaoPaulo <jpdsrocha17@gmail.com>
2025-03-22 13:11:36 -0300

    Feat permissao delete

--- app/Livewire/Medication/Delete.php
+++ app/Livewire/Medication/Delete.php
@@ -3,11 +3,36 @@
 namespace App\Livewire\Medication;
 
 use Livewire\Component;
+use App\Models\Medication;
+use Livewire\Attributes\On;
+use Mary\Traits\Toast;
 
 class Delete extends Component
 {
+    use Toast;
+    public Medication $medication;
+
+    public bool $modal = false;
+
     public function render()
+
     {
         return view('livewire.medication.delete');
+    }
+    #[On('medication::delete')]
+    public function openConfirmationFor(int $id): void
+    {
+        $this->medication = Medication::find($id);
+        $this->modal      = true;
+    }
+
+    public function destroy(): void
+    {
+        $this->autorize('write_medications');
+
+        $this->medication->delete();
+        $this->success('Medicação excluida com sucesso!');
+        $this->dispatch('medication::deleted');
+        $this->reset();
     }
 }
--- resources/views/livewire/medication/delete.blade.php
+++ resources/views/livewire/medication/delete.blade.php
@@ -1,3 +1,16 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    
+    @if ($modal)
+        <x-modal wire:model="modal" :title="'Confirmação de Exclusão'" subtitle="Tem certeza que deseja excluir esta medicação?" separator></x-modal>
+            <x-form>
+                <div class="space-y-2">
+                    <x-input readonly label="Nome" :value="$medication?->name" />
+                </div>
+                <x-slot:actions>
+                    <x-button label="Cancelar" @click="$wire.modal = false" />
+                    <x-button label="Deletar" class="btn-warning" wire:click="destroy" />
+                </x-slot:actions>
+            </x-form>
+        </x-modal>
+
 </div>
