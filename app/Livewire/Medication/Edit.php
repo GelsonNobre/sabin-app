1a6ac82d1870e54bb72e6c18dcf93f1d45115d02
Gelson Nobre <gelsonnobre2731@gmail.com>
2025-03-25 09:55:59 -0300

    WIP: alterações no formulário de edição de medicação

--- app/Livewire/Medication/Edit.php
+++ app/Livewire/Medication/Edit.php
@@ -3,11 +3,59 @@
 namespace App\Livewire\Medication;
 
 use Livewire\Component;
+use App\Models\Medication;
+use App\Livewire\Forms\MedicationForm;
+use Illuminate\Contracts\View\{Factory, View};
+use Illuminate\Foundation\Application;
+use Mary\Traits\Toast;
+
+
 
 class Edit extends Component
 {
-    public function render()
+    use Toast;
+
+    public $indicationTypes, $aplicationTypes = [];
+
+    public MedicationForm $form;
+
+    public function render(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
     {
         return view('livewire.medication.edit');
+    }
+
+    public function mount(int $id): void
+    {
+        $medication = Medication::find($id);
+        $this->form->setObject($medication);
+        //$this->indicationTypes = $this->indicationTypes();
+        //$this->aplicationTypes = $this->aplicationTypes();
+    }
+
+    public function indicationTypes()
+    {
+        return collect([
+            (object) ['id' => 'NL', 'name' => 'Selecione'],
+            (object) ['id' => 'adulto', 'name' => 'Adulto'],
+            (object) ['id' => 'infantil', 'name' => 'Infantil'],
+        ]);
+    }
+
+    public function aplicationTypes()
+    {
+        return collect([
+            (object) ['id' => 'NL', 'name' => 'Selecione'],
+            (object) ['id' => 'IM', 'name' => 'Intramuscular'],
+            (object) ['id' => 'IV', 'name' => 'Intravenosa'],
+            (object) ['id' => 'SC', 'name' => 'Subcutanea'],
+            (object) ['id' => 'IA', 'name' => 'Intra-articular'],
+        ]);
+    }
+
+    public function submit(): void
+    {
+        $this->form->store();
+        $this->success('Nova medicação criada com sucesso!');
+        $this->redirect('/medications');
     }
 }
--- resources/views/livewire/medication/edit.blade.php
+++ resources/views/livewire/medication/edit.blade.php
@@ -1,3 +1,30 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <!-- HEADER -->
+    <x-header title="Nova Medicação" separator progress-indicator>
+        <x-slot:actions>
+            <x-button label="Cancelar" link="{{ route('medications') }}" />
+        </x-slot:actions>
+    </x-header>
+
+    <x-form wire:submit.prevent="submit">
+        <x-card title="Nome e Fabricante" separator>
+            <x-form.row cols="2">
+                <x-input label="Nome" placeholder="Nome" wire:model="form.name" />
+                <x-input label="Fabricante" placeholder="Fabricante" wire:model="form.producer" />
+            </x-form.row>
+        </x-card>
+
+        <x-card title="Detalhes" separator>
+                <x-form.row cols="3">
+                    <x-select label="Tipo de Indicação" :options="$indicationTypes" wire:model="form.age_type" />  
+                    <x-select label="Tipo de Aplicação" :options="$aplicationTypes"  wire:model="form.type_of_aplication"  />
+                    <x-input label="Valor da Aplicação" placeholder="0,00" wire:model.lazy="form.price" prefix="R$" money locale="pt-BR" />
+                </x-form.row>
+        </x-card>
+        <x-slot:actions>
+            <x-button label="Cancelar" link="{{ route('medications') }}" />
+            <x-button label="Salvar" class="btn-primary" type="submit" spinner="save" />
+        </x-slot:actions>
+    </x-form>
 </div>
+
