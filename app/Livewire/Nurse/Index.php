81f8c1889ef9aead6fad8530b35bd35f111ac498
JoaoPaulo <jpdsrocha17@gmail.com>
2025-04-07 19:42:53 -0300

    Nurse migration

---
+++ app/Livewire/Nurse/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Nurse;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.nurse.index');
+    }
+}
