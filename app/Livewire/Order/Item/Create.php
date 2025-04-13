15aee5da60175ee17ced07f0aec747bc265d1675
Gelson Nobre <gelsonnobre2731@gmail.com>
2025-04-13 12:43:07 -0300

    Feat: Init Order Componentes

--- app/Livewire/Order/Item/Create.php
+++ app/Livewire/Order/Item/Create.php
@@ -2,12 +2,42 @@
 
 namespace App\Livewire\Order\Item;
 
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Mary\Traits\Toast;
 
 class Create extends Component
 {
-    public function render()
+    use Toast; 
+
+    public ?int $id = null;
+
+    #[Validate('required', as: 'item')]
+    public ?string $item = null;
+
+    public array $medications;
+
+    public array $items = [];
+
+
+    public function render(): View
     {
         return view('livewire.order.item.create');
+    }
+
+
+    public function store(): void
+    {
+        $data = $this->validate();
+
+        $data['id'] = $this->id;
+        $data['name'] = $this->name;
+
+        $this->dispatch('order::item::created', object: $data)->to('order.item.index');
+
+        $this->success('Nova medicação adicionada com sucesso!');
+
+        $this->reset('id', 'item', 'value');
     }
 }
--- app/Livewire/Order/Item/Index.php
+++ app/Livewire/Order/Item/Index.php
@@ -2,12 +2,77 @@
 
 namespace App\Livewire\Order\Item;
 
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Modelable;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public function render()
+    #[Modelable]
+    public array $items = [];
+
+    public float $total = 0;
+
+
+    public function render(): View
     {
         return view('livewire.order.item.index');
+    }
+
+    public function mount(): void
+    {
+
+        if ($this->items) {
+            $this->updateTotal();
+        }
+
+    }
+
+    public function boot(): void
+    {
+        if (empty($this->items)) {
+            $this->reset('total');
+        }
+    }
+
+    private function updateTotal(): void
+    {
+        $this->total = array_sum(array_column($this->items, 'value'));
+
+    }
+
+    /** @param array<string, mixed> $object */
+    #[On('order::item::created')]
+    public function store(array $object): void
+    {
+
+        if (!is_array($this->items)) {
+            $this->items = [];
+        }
+
+        $this->items[$object['id']] = $object;
+
+        $this->updateTotal();
+    }
+
+    /**
+     * @param array<string, mixed> $object
+     */
+    #[On('order::item::updated')]
+    public function update(array $items): void
+    {
+        foreach ($items as $id => $item) {
+            $this->items[$id] = $item;
+        }
+
+        $this->updateTotal();
+    }
+
+    #[On('order::item::deleted')]
+    public function delete(int $key): void
+    {
+        unset($this->items[$key]);
+        $this->updateTotal();
     }
 }
